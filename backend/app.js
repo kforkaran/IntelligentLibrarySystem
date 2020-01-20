d2d4ae2c63ea6d302208a78d6cfadf78a6d2cfd2
Anirudh <anirudhganwal06@gmail.com>
2020-01-20 23:50:41 +0530

    created auth route

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const keys = require("./config/keys");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+app.use("/api/admin", authRoutes);
+
 mongoose
   .connect(
-    "mongodb+srv://demo:demo@cluster0-7m0ym.mongodb.net/test?retryWrites=true&w=majority",
+    keys.mongoURI,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(res => {
-    const server = app.listen(3000);
+    const server = app.listen(8000);
     console.log("connected!");
   })
   .catch(err => {
