4bdebff04ef8ccfbd45382d89dfe5af2f0feba63
Rekhansh Priyadarshi <priyadarshirekhansh@gmail.com>
2020-01-21 11:25:19 +0530

    Assistant tweaks

--- backend/routes/assistant.js
+++ backend/routes/assistant.js
@@ -6,25 +6,47 @@ const Book = require('../models/book');
 const app = dialogflow();
 
 app.intent("Get Personal Details", (conv, { roll }) => {
-  console.log("Hello");
   return Student.findOne({ rollNo: roll })
     .then(async student => {
       const promises = [];
+
+      if(!student)
+        return conv.ask("No record found for this roll number");
 
       student.booksIssued.forEach(book =>
         promises.push(Book.findOne({ id: book.bookId })));
 
       const books = await Promise.all(promises);
 
-      const items = books.map(book => {
+      // console.log(books);
+      // const items = {
+      //   '0': {
+      //     title: 'Lol0',
+      //     description: 'Issued On: Tue Jan 21 2020 05:30:00 GMT+0530 (India Standard Time)<br>Due On: Wed Jan 22 2020 05:30:00 GMT+0530 (India Standard Time)'
+      //   },
+      //   '1': {
+      //     title: 'Lol1',
+      //     description: 'Issued On: Tue Jan 21 2020 05:30:00 GMT+0530 (India Standard Time)<br>Due On: Wed Jan 22 2020 05:30:00 GMT+0530 (India Standard Time)'
+      //   },
+      //   '2': {
+      //     title: 'Lol2',
+      //     description: 'Issued On: Tue Jan 21 2020 05:30:00 GMT+0530 (India Standard Time)<br>Due On: Wed Jan 22 2020 05:30:00 GMT+0530 (India Standard Time)'
+      //   },
+      //   '3': {
+      //     title: 'Lol3',
+      //     description: 'Issued On: Tue Jan 21 2020 05:30:00 GMT+0530 (India Standard Time)<br>Due On: Wed Jan 22 2020 05:30:00 GMT+0530 (India Standard Time)'
+      //   }
+      // };
+
+      books.forEach((book, i) => {
         const studentBookDetails = student.booksIssued.find(b => b.bookId == book.id);
-        return {
+        items[i] = {
           title: book.volumeInfo.title,
-          description: "Issued On: " + studentBookDetails.issueDate
-            + "\nDue On: " + studentBookDetails.dueDate
+          description: "Issued On: " + studentBookDetails.issueDate.toDateString()
+            + "<br>Due On: " + studentBookDetails.dueDate.toDateString()
         }
       });
-
+      console.log(items);
       conv.ask("Here are the list of books issued by you...");
       conv.ask(new List({
         title: "List of Issued Books",
